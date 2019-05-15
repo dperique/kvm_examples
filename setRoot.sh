c9ffdfd612083271bab0800f4c79c615595ff3c7
Dennis Periquet <Dennis.Periquet@ibm.com>
2019-05-15 09:28:09 -0400

    Comments and set password

--- setRoot.sh
+++ setRoot.sh
@@ -13,11 +13,19 @@ set timeout 60
 log_user 1
 
 spawn ssh centos@$aHost
-
 expect "password:" { send "$aPass\r" }
+
+# Turn on root login for ssh
+#
 expect "$ " { send "sudo su\r" }
 expect "# " { send "echo \"PermitRootLogin yes\" >> /etc/ssh/sshd_config\r" }
 expect "# " { send "service sshd restart\r" }
+
+# Set root password to aPa
+#
+expect "# " { send "passwd\r" }
+expect "password: " { send "$aPass\r" }
+expect "password: " { send "$aPass\r" }
 
 expect "# " { send "exit\r" }
 expect "$ " { send "exit\r" }
